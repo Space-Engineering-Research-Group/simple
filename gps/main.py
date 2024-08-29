81c6e0798a9dc8569a4fba0419cce74bef461bf0
hototogisut <ookaminokumo123@gmail.com>
2024-08-30 00:03:26 +0900

    後で追加するものある

--- gps/main.py
+++ gps/main.py
@@ -1,21 +1,50 @@
-from abc import ABC,abstractmethod
-
+import abc
 import serial
 import micropyGPS
 
-class IGps(ABC):
-    @abstractmethod
+class IGps(abc.ABC):
+    @abc.abstractmethod
     def run_gps(self):
-        pass
+        
+        raise NotImplementedError()
 
-    @abstractmedhod
-    def get_coordinates(self) ->(float,float): # type: ignore
-        pass
-
+    @abc.abstractmethod
+    def get_coordinates(self) -> tuple[float, float]:
+       
+        raise NotImplementedError()
 
 class Gps(IGps):
     def __init__(self):
-        self.ser=serial.Serial('/dev/serial0', baudrate=9600, timeout=1)
-    
-    def get_coordinates(self) ->(float,float): #type:ignore
-        self.deta=self.ser.readline()
+        #ここのポートと通信速度、タイムアウトの値は仮に設定した
+        self.__uart = serial.Serial('/dev/serial0', 9600, timeout=10)
+        self.__gps = micropyGPS.MicropyGPS(9, "dd")
+        self.__data_buffer = ""
+
+    def run_gps(self):
+      
+        while True:
+            try:
+                sentence = self.__uart.readline()
+                
+                if sentence:
+                    self.__data_buffer += sentence.decode('ascii', errors='ignore')
+                   
+                    while '\n' in self.__data_buffer:
+                        sentence, self.__data_buffer = self.__data_buffer.split('\n', 1)
+
+                        for x in sentence:
+                            if 10 <= ord(x) <= 126:
+                                if self.__gps.update(x):
+                                    # 更新されたGPSデータをログするなどの処理を後で書く
+                                    pass
+
+            except Exception as e:
+                print(f"GPSからの読み取りエラー: {e}")
+
+    def get_coordinates(self) -> tuple[float, float]:
+
+       #２つの変数がnotNoneなら値を入れ、elseならNoneを入れる
+        latitude = self.__gps.latitude[0] if self.__gps.latitude[0] is not None else None
+        longitude = self.__gps.longitude[0] if self.__gps.longitude[0] is not None else None
+
+        return latitude, longitude
