d7f1f4dcd4e78da83fff9966f810bf9630f0198b
pekkotarou <pekkotaisa@gmail.com>
2024-08-30 17:27:18 +0900

    releaseを加えた

--- camera/main.py
+++ camera/main.py
@@ -12,17 +12,22 @@ class ICamera(ABC):
 class Camera(ICamera):
     def __init__(self):
         self.capture=cv2.VideoCapture(0)
-    
+        if not self.capture.isOpened():
+            raise RuntimeError("カメラを開けませんでした")
     def get_size(self):
         width =self.capture.get(cv2.CAP_PROP_FRAME_WIDTH)
         height=self.capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
         return (width,height)
 
     def get_frame(self):
-        ret,frame=self.cap.read()
+        ret,frame=self.capture.read()
         if ret:
             return frame
         else:
             print("カメラから映像を取得できません")
-            #エラー処理の内容は後で決める
-            raise 
+            #エラーの内容はchatgptのやつコピペしただけ
+            raise RuntimeError("カメラから映像を取得できません")
+
+    def release(self):
+        if self.capture.isOpened():
+            self.capture.release
