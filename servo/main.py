383685cff2751bc10debdbdfb003382036a38f67
hototogisut <ookaminokumo123@gmail.com>
2024-09-03 12:18:40 +0900

    do

--- servo/main.py
+++ servo/main.py
@@ -1,1 +1,11 @@
-pass
+import abc
+from GPIOZERO from servo
+
+class Iservo(abc.ABC):
+     @abc.abstractmethod
+     def rotate(self):
+          pass
+     
+class Servo(Iservo):
+     def __init__(self):
+               self._servo = servo 
