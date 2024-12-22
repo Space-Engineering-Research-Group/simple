59831719035476f29ae69786762ff8ce40e01b67
pekkotarou <pekkotaisa@gmail.com>
2024-12-23 00:28:58 +0900

    そくどへんか

--- motor/main.py
+++ motor/main.py
@@ -1,8 +1,5 @@
 from gpiozero import PWMOutputDevice,DigitalOutputDevice
-from gpiozero.pins.pigpio import PiGPIOFactory
 from abc import ABC,abstractmethod
-
-factory = PiGPIOFactory()
 
 class Imotor(ABC):
     @abstractmethod
@@ -24,7 +21,7 @@ class Imotor(ABC):
     
     
 class Motor(Imotor):
-    def __init__(self,rdir_1,rdir_2,rPWM,ldir_1,ldir_2,lPWM):
+    def __init__(self,rdir_1,rdir_2,rPWM,ldir_1,ldir_2,lPWM,factory):
         self.right_in1=DigitalOutputDevice(rdir_1,pin_factory=factory)
         self.right_in2=DigitalOutputDevice(rdir_2,pin_factory=factory)
         self.right_PWM=PWMOutputDevice(rPWM,pin_factory=factory)
@@ -33,29 +30,42 @@ class Motor(Imotor):
         self.left_in2=DigitalOutputDevice(ldir_2,pin_factory=factory)
         self.left_PWM=PWMOutputDevice(lPWM,pin_factory=factory)
 
-        self.right_PWM.value=204/255
-        self.left_PWM.value=204/255
+        self.right_PWM.value=1
+        self.left_PWM.value=1
         
-    def forward(self):
+    def forward(self,speed):
+        self.right_PWM.value=speed
+        self.left_PWM.value=speed
+
         self.right_in1.on()
         self.right_in2.off()
 
         self.left_in1.off()
         self.left_in2.on()
 
-    def turn_right(self):
+    def turn_right(self,speed):
+        self.right_PWM.value=speed
+        self.left_PWM.value=speed
+
         self.right_in1.off()
         self.right_in2.on()
 
         self.left_in1.off()
         self.left_in2.on()
 
-    def turn_left(self):
+    def turn_left(self,speed):
+        self.right_PWM.value=speed
+        self.left_PWM.value=speed
+
         self.right_in1.on()
         self.right_in2.off()
 
         self.left_in1.on()
         self.left_in2.off()
+    
+    def stop(self):
+        self.right_PWM.value=0
+        self.left_PWM.value=0
 
     #main.pyファイルを書き直しておく
     def release(self):
