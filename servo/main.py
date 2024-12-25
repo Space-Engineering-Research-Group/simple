b205278de2879d01b4b50828a98ee6fe1f0a31f4
pekkotarou <pekkotaisa@gmail.com>
2024-12-25 14:43:53 +0900

    例外処理の完成

--- servo/main.py
+++ servo/main.py
@@ -1,16 +1,22 @@
 import abc
 from gpiozero import Servo
+from time import sleep
 
 class Iservo(abc.ABC):
      @abc.abstractmethod
      def rotate(self):
           pass
+
+     def stop(self):
+          pass
      
 class Servo(Iservo):
      def __init__(self,pin):
-          #maxpulseとminpulseも必要があれば指定
           self.servo=Servo(pin)
      
      def rotate(self):
           #必要に応じて変える
-          self.servo.max()
+          self.servo.value=1
+
+     def stop(self):
+          self.servo.value=0
