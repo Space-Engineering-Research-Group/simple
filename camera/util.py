27c0050c546607624aae30599313f424db47ff2f
pekkotarou <pekkotaisa@gmail.com>
2024-12-25 17:23:19 +0900

    途中経過

--- camera/util.py
+++ camera/util.py
@@ -19,6 +19,22 @@ def find_cone(frame,lower_red1,upper_red1,lower_red2,upper_red2):
     mask=cv2.Canny(mask,80.0,175.0)
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+def find_parachute(frame,lower_yellow,upper_yellow):
+    img_yuv=cv2.cvtColor(frame,cv2.COLOR_BGR2YUV)
+    clahe=cv2.createCLAHE(clipLimit=2.0,tileGridSize=(8,8))
+    img_yuv[:,:,0]=clahe.apply(img_yuv[:,:,0])
+    img_bgr = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
+    img_hsv=cv2.cvtColor(img_bgr,cv2.COLOR_BGR2HSV)
+
+    mask = cv2.inRange(img_hsv, lower_yellow, upper_yellow)       
+    mask = cv2.medianBlur(mask, 11)
+    mask[0, :] = 0            
+    mask[-1, :] = 0            
+    mask[:, 0] = 0            
+    mask[:, -1] = 0 
+    mask=cv2.Canny(mask,80.0,175.0)
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
 
     if contours:
         # 最大面積のコンターを返す
@@ -33,6 +49,15 @@ def judge_cone(contour,frame_area):
     if raito >= 0.04:
         return True
     return False
+
+def judge_parachute(contour,frame_area):
+    area=cv2.contourArea(contour)
+    raito=(area/frame_area)*100
+    #個々の値も適当
+    if raito >= 0.04:
+        return True
+    return False
+    
 
         
 def get_distance(contour,x):
