4841dee9fc5099a944e69657ef4bc226ed2908b7
hototogisut <ookaminokumo123@gmail.com>
2024-12-29 14:04:38 +0900

    新しい関数追加

--- xbee/util.py
+++ xbee/util.py
@@ -1,12 +1,2 @@
-def xbee_jude(jude):
-    #feeds1 
-    #1_cds、2_gps 、3_camara、4_motor、5_sarvo、6_xbee、7_故障した部品、8_エラー分
-    #feeds2-A
-    #1_明るさ、2_落下判定、3_故障した部品、4_エラー分
-    #feeds2-B
+def xbee_raspy(data):
     
-    #feeds3
-    #1_高度、2_着地判定、3_故障した部品、4_エラー分
-    #feeds4
-    #1_サーボ
-
