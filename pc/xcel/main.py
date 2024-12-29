ab6b5bf2c885ecc29df20bd78bb415ff721a98cc
hototogisut <ookaminokumo123@gmail.com>
2024-12-29 22:53:39 +0900

    クラス化した

--- pc/xcel/main.py
+++ pc/xcel/main.py
@@ -1,25 +1,24 @@
 import xlwings as xw
 import os
 
-file_path = r"C:/Users/pekko/OneDrive/ドキュメント/rog.xlsx"
+class Xcel():
+    def open_workbook(self,file_path):
+        self.app = xw.App(visible=True)
+        try:
+            if not os.path.exists(file_path):
+                raise FileNotFoundError(f"指定されたファイル {file_path} が見つかりません。")
+            self.workbook = self.app.books.open(file_path)
+            sheet = self.workbook.sheets[0]
+            return self.app, self.workbook, sheet
+        except Exception as e:
+            self.app.quit()
+            raise e
 
-def open_workbook(file_path):
-    app = xw.App(visible=True)
-    try:
-        if not os.path.exists(file_path):
-            raise FileNotFoundError(f"指定されたファイル {file_path} が見つかりません。")
-        workbook = app.books.open(file_path)
-        sheet = workbook.sheets[0]
-        return app, workbook, sheet
-    except Exception as e:
-        app.quit()
-        raise e
 
-
-def delete(workbook,app):
-    if workbook in locals() and workbook is not None:
-        workbook.save()
-    if workbook in locals() and workbook is not None:
-        workbook.close()  # workbook を閉じる
-    if app in locals() and app is not None:
-        app.quit()
+    def delete(self):
+        if self.workbook in locals() and self.workbook is not None:
+            self.workbook.save()
+        if self.workbook in locals() and self.workbook is not None:
+            self.workbook.close()  # workbook を閉じる
+        if self.app in locals() and self.app is not None:
+            self.app.quit()
