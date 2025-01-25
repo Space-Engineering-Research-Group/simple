3598880d91908beb9a27f806679babf384fb0aa4
pekkotarou <pekkotaisa@gmail.com>
2025-01-25 17:03:08 +0900

    matigaisyuusei

--- motor/main.py
+++ motor/main.py
@@ -90,7 +90,7 @@ class Motor(Imotor):
                 self.left_handle_error(error)
 
             finally:
-                if (len(self.right_error_counts)and self.a==0)or 5 in self.left_error_counts:
+                if (len(self.left_error_counts)and self.a==0)or 5 in self.left_error_counts:
                     self.left_log_errors()
                     break
             sleep(1)
@@ -185,7 +185,7 @@ class Motor(Imotor):
                 error = f"left motor error during backward motion--detail {e}"
                 self.left_handle_error(error)
             finally:
-                if (len(self.right_error_counts)and self.a==0)or 5 in self.left_error_counts:
+                if (len(self.left_error_counts)and self.a==0)or 5 in self.left_error_counts:
                     self.left_log_errors()
                     break
             sleep(1)
@@ -229,7 +229,7 @@ class Motor(Imotor):
                 error = f"left motor error during right turn--detail {e}"
                 self.left_handle_error(error)
             finally:
-                if (len(self.right_error_counts)and self.a==0) or 5 in self.right_error_counts:
+                if (len(self.left_error_counts)and self.a==0) or 5 in self.left_error_counts:
                     self.right_log_errors()
                     break
             sleep(1)
@@ -275,7 +275,7 @@ class Motor(Imotor):
                 error = f"left motor error during left turn--detail {e}"
                 self.left_handle_error(error)
             finally:
-                if (len(self.right_error_counts)and self.a==0)or 5 in self.left_error_counts:
+                if (len(self.left_error_counts)and self.a==0)or 5 in self.left_error_counts:
                     self.left_log_errors()
                     break
             sleep(1)
