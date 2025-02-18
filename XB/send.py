b02b2b70cd154a6e346e02721a9b0b9adc721ae8
hototogisut <ookaminokumo123@gmail.com>
2025-02-18 14:43:42 +0900

    分からん

--- XB/send.py
+++ XB/send.py
@@ -58,10 +58,43 @@ class XBeeCommunication(IXBeeDevice):
                 device.send_data(remote_device, self.DATA_TO_SEND)
 
                 print("Success")
-
-            finally:
+                self.a = 0
+                break
+            except Exception as e:
+                error = f"Failed to open the serial port:: /dev/serial0. Ensure the port is not busy or unavailable.:--detail{e}"
+                self.handle_error(error)
                 if device is not None and device.is_open():
                     device.close()
+            finally:
+                if (len(self.error_messages)and self.a==0)or 5 in self.error_counts:
+                        if 5 in self.error_counts:
+                            if device is not None and device.is_open():
+                                device.close()
+                        self.xbee_errors()   
+                        break  
+                
+    def log_errors(self):
+        list=[]
+        for count,message in zip(self.error_counts,self.error_messages):
+            list.append(f"{count}*{message}")
+        if self.a==0:
+            self.error_log=",".join(list)
+        elif 5 in self.error_counts:
+            index=self.error_counts.index(5)
+            result=list[:index]+list[index+1:]
+            result=",".join(result)
+            self.error_log=f"xbee:Error--{list[index]} other errors--{result}"
+        if self.ini==False:    
+            raise RuntimeError
+
+    def handle_error(self,error):
+        if str(error) not in self.error_messages:
+            self.error_messages.append(str(error))
+            self.error_counts.append(1)
+        else:
+            index = self.error_messages.index(str(error))
+            self.error_counts[index] += 1
+            
 
 
 if __name__ == '__main__':
