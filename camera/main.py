e2472fa2dec7af179c4b2f6ddda4ba645a540c1a
pekkotarou <pekkotaisa@gmail.com>
2025-02-22 18:00:49 +0900

    henkou

--- camera/main.py
+++ camera/main.py
@@ -2,6 +2,7 @@ import cv2
 from picamera2 import Picamera2
 from abc import ABC,abstractmethod
 from time import sleep
+from datetime import datetime as dt
 
 class ICamera(ABC):
     @abstractmethod
@@ -37,6 +38,16 @@ class Camera(ICamera):
                 )
                 self.capture.configure(config)
                 self.capture.start()
+
+                # 出力ファイル名設定
+                date = dt.now().strftime("%Y%m%d_%H%M")
+                path = "./" + date + ".mp4"
+
+                # 出力ファイル設定
+                fourcc = cv2.VideoWriter_fourcc(*'mp4v')  
+                self.out = cv2.VideoWriter(path, fourcc, fps, (width, height))
+                if not self.out.isOpened():
+                    raise RuntimeError("Failed to open VideoWriter")
                 self.a = 0
                 break
             except Exception as e:
@@ -75,7 +86,78 @@ class Camera(ICamera):
                     self.log_errors()
             sleep(1)
                 
+    def frame_hozon(self,frame):
+        self.error_counts = []
+        self.error_messages = []
+        self.error_log = "camera:Error"
+        self.a = 1
+        self.ini = False
 
+        while True:
+            try:
+                self.out.write(frame)
+                self.a = 0
+                break
+            except Exception as e:
+                error = f"camera error during frame hozon -- detail {e}"
+                self.handle_error(error)
+            finally:
+                if (len(self.error_counts) and self.a == 0) or 5 in self.error_counts:
+                    self.log_errors()              
+            sleep(1)
+
+
+    def cone_hozon(self, frame, contour):
+        self.error_counts = []
+        self.error_messages = []
+        self.error_log = "camera:Error"
+        self.a = 1
+        self.ini = False
+
+        while True:
+            try:
+                img=cv2.drawContours(frame, contour, -1, (0, 255, 0), 3)
+                self.out.write(img)
+                self.a = 0
+                break
+            except Exception as e:
+                error = f"camera error during cone hozon -- detail {e}"
+                self.handle_error(error)
+            finally:
+                if (len(self.error_counts) and self.a == 0) or 5 in self.error_counts:
+                    self.log_errors()
+            sleep(1)
+    
+    def parachute_hozon(self, frame,yellow_lower,yellow_upper):
+        self.error_counts = []
+        self.error_messages = []
+        self.error_log = "camera:Error"
+        self.a = 1
+        self.ini = False
+
+        while True:
+            try:
+                img_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+                mask = cv2.inRange(img_hsv, yellow_lower, yellow_upper) 
+                mask = cv2.medianBlur(mask, 11)
+                mask[0, :] = 0            
+                mask[-1, :] = 0            
+                mask[:, 0] = 0            
+                mask[:, -1] = 0 
+                mask=cv2.Canny(mask,80.0,175.0)
+                contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                if contours:
+                    cv2.drawContours(frame, contours, -1, (255,0, 0), 3)
+                self.out.write(frame)
+                self.a = 0
+                break
+            except Exception as e:
+                error = f"camera error during parachute hozon -- detail {e}"
+                self.handle_error(error)
+            finally:
+                if (len(self.error_counts) and self.a == 0) or 5 in self.error_counts:
+                    self.log_errors()
+            sleep(1)
     
 
     def release(self):
