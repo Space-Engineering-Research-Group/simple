60345422e14b3e39890eac96789bdc0d2430207c
hototogisut <ookaminokumo123@gmail.com>
2025-02-25 18:18:52 +0900

    成功コード

--- XB/receive.py
+++ XB/receive.py
@@ -1,4 +1,5 @@
 
+#pcで実行
 from digi.xbee.devices import XBeeDevice
 
 # TODO: Replace with the serial port where your local module is connected to.
@@ -6,6 +7,11 @@ PORT = "COM9"
 # TODO: Replace with the baud rate of your local module.
 BAUD_RATE = 9600
 
+def is_integer(s):
+    try:
+        return int(s)
+    except ValueError:
+        return s  # 整数に変換できない場合はそのまま返す
 
 def main():
     print(" +-----------------------------------------+")
@@ -18,8 +24,14 @@ def main():
         device.open()
 
         def data_receive_callback(xbee_message):
-            print("From %s >> %s" % (xbee_message.remote_device.get_64bit_addr(),
-                                     xbee_message.data.decode()))
+            data_string = xbee_message.data.decode()
+            print("From %s >> %s" % (xbee_message.remote_device.get_64bit_addr(), data_string))
+
+            # 受信したデータをリストに変換
+            data_list_str = data_string.split(',')
+            data_list = list(map(is_integer, data_list_str))
+
+            print("Converted list:", data_list)
 
         device.add_data_received_callback(data_receive_callback)
 
@@ -30,11 +42,5 @@ def main():
         if device is not None and device.is_open():
             device.close()
 
-
 if __name__ == '__main__':
     main()
-
-
-
-
-
