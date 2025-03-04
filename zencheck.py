e1080f9d885e3fb56c6f229a4e5ab2327b20fbc9
pekkotarou <pekkotaisa@gmail.com>
2025-03-04 14:19:02 +0900

    o

--- zencheck.py
+++ zencheck.py
@@ -11,14 +11,8 @@ try:
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    from camera import *
-    import numpy as np
     from gps import *
-    from motor import *
-    from gpiozero.pins.pigpio import PiGPIOFactory
-    from cds import *
     from XB import *
-    from servo import *
     from raspberry_log import *
     from time import sleep,time
     from datetime import datetime, timedelta, timezone
@@ -48,36 +42,6 @@ try:
     ins_error_tool=[]
     ins_error=[]
 
-    #光センサで明るいことを検知してから落下するまでにかかると想定する時間
-    fall_time=60
-    #光センサで判断できなかった場合に置ける処理が起動してから落下するまでにかかるだろうとする時間
-    land_time=480
-
-    try:
-        cds=Cds()
-    finally:
-        if len(cds.error_counts)>0:
-            ins_error.append(cds.error_log)   
-            if  5 in cds.error_counts:
-                ins_error_tool.append("cds")
-                tools[0]=False   
-
-
-    #明るさの閾値は曇りの日に明るさを取得して決める
-    brightness_threshold=0.3
-    #ピンの値は回路班が後で決めるので仮の値
-    factory = PiGPIOFactory()
-    try:
-        servo=Myservo(12,factory)
-    finally:
-        if len(servo.error_counts)>0:
-            ins_error.append(servo.error_log)
-            if 5 in servo.error_counts:
-                ins_error_tool.append("servo")
-                tools[4]=False
-    
-    #サーボモーターが回転する時間
-    srote_time=60
 
     try:
         gps=Gps()
@@ -102,62 +66,6 @@ try:
     #カメラが壊れてplanBになった時の成功
     B_x2 = 0.5
 
-
-    #設定的に一番低そうなこれにする。
-    width=640
-    height=480
-    fps=10
-    center=width//2
-    frame_area=width*height
-    #カメラの画角（実験値）
-    view_angle=120
-    #ここは機体が組みあがった後に実験して決める
-    parea_threshold=0.04
-    try:
-        camera=Camera(width,height,fps)
-    finally:
-        if len(camera.error_counts)>0:
-            ins_error.append(camera.error_log)
-            if 5 in camera.error_counts:
-                ins_error_tool.append("camera")
-                tools[2]=False
-
-    #ここの具体的な値はコーンの検査をして考える。
-    lower_red1 = np.array([0, 100, 70])   # 下の範囲1 (0〜10度)
-    upper_red1 = np.array([10, 255, 255])  # 上の範囲1
-    lower_red2 = np.array([170, 100, 70]) # 下の範囲2 (170〜180度)
-    upper_red2 = np.array([180, 255, 255]) # 上の範囲2
-
-    #パラシュートの黄色をしっかり検出出来るようにする。
-    lower_yellow=np.array([20, 100, 100])
-    upper_yellow=np.array([40, 255, 255])
-
-
-    #pin,pwmの値は決まった
-    rdir_1=19
-    rdir_2=26
-    rPWM=13
-    ldir_1=23
-    ldir_2=24
-    lPWM=18
-    #機体の回転速度208度/s
-    turn_speed=208
-    #機体の前進速度20cm/s
-    go_speed=20
-    #９０度回転するときの待機時間
-    sttime_90=90/turn_speed
-
-    try:
-        motors=Motor(rdir_1,rdir_2,rPWM,ldir_1,ldir_2,lPWM,factory)
-    finally:
-        if len(motors.left_error_counts)>0 or len(motors.right_error_counts)>0:
-            ins_error.append(motors.error_log)
-            if 5 in motors.left_error_counts:
-                ins_error_tool.append("left motor")
-                tools[3]=False
-            if 5 in motors.right_error_counts:
-                ins_error_tool.append("right motor")
-                tools[3]=False
 
     try:
         xbee=XBee()
@@ -223,207 +131,8 @@ try:
         notice_log=[9,ward]
         rog(notice_log)
       
-
-    def mforward(wait_time):
-        #フェーズ、時間、故障した部品、エラー文      
-        motor_log=[10,None,[],None]
-        try:
-            motors.forward()
-            if wait_time>0:
-                sleep(wait_time)
-        except RuntimeError:
-            tools[3]=False
-            import sys
-            sys.exit(1)
-        finally:
-            if len(motors.right_error_counts) or len(motors.left_error_counts):
-                motor_log[1]=mget_time()
-                motor_log[-1]=motors.error_log
-                if 5 in motors.right_error_counts:
-                    motor_log[-2].append("right motor")
-                if 5 in motors.left_error_counts:
-                    motor_log[-2].append("left motor")
-                rog(motor_log)
-
-
-    def mbackward(wait_time):
-        #フェーズ、時間、故障した部品、エラー文
-        motor_log=[10,None,[],None]
-        try:
-            motors.backward()
-            if wait_time>0:
-                sleep(wait_time)
-        except RuntimeError:
-            tools[3]=False
-            import sys
-            sys.exit(1)
-        finally:
-            if len(motors.right_error_counts) or len(motors.left_error_counts):
-                motor_log[1]=mget_time()
-                motor_log[-1]=motors.error_log
-                if 5 in motors.right_error_counts:
-                    motor_log[-2].append("right motor")
-                if 5 in motors.left_error_counts:
-                    motor_log[-2].append("left motor")
-                rog(motor_log)
-
-    def mturn_left(wait_time):
-        #フェーズ、時間、故障した部品、エラー文
-        motor_log=[10,None,[],None]
-        try:
-            motors.turn_left()
-            if wait_time>0:
-                sleep(wait_time)
-        except RuntimeError:
-            tools[3]=False
-            import sys
-            sys.exit(1)
-        finally:
-            if len(motors.right_error_counts) or len(motors.left_error_counts):
-                motor_log[1]=mget_time()
-                motor_log[-1]=motors.error_log
-                if 5 in motors.right_error_counts:
-                    motor_log[-2].append("right motor")
-                if 5 in motors.left_error_counts:
-                    motor_log[-2].append("left motor")
-                rog(motor_log)
-
-
-    def mturn_right(wait_time):
-        motor_log=[10,None,[],None]
-        try:
-            motors.turn_right()
-            if wait_time>0:
-                sleep(wait_time)
-        except RuntimeError:
-            tools[3]=False
-            import sys
-            sys.exit(1)
-        finally:
-            if len(motors.right_error_counts) or len(motors.left_error_counts):
-                motor_log[1]=mget_time()
-                motor_log[-1]=motors.error_log
-                if 5 in motors.right_error_counts:
-                    motor_log[-2].append("right motor")
-                if 5 in motors.left_error_counts:
-                    motor_log[-2].append("left motor")
-                rog(motor_log)
-
-    def mstop():
-        motor_log=[10,None,[],None]
-        try:
-            motors.stop()
-        except RuntimeError:
-            tools[3]=False
-            import sys
-            sys.exit(1)
-        finally:
-            if len(motors.right_error_counts) or len(motors.left_error_counts):
-                motor_log[1]=mget_time()
-                motor_log[-1]=motors.error_log
-                if 5 in motors.right_error_counts:
-                    motor_log[-2].append("right motor")
-                if 5 in motors.left_error_counts:
-                    motor_log[-2].append("left motor")
-                rog(motor_log)
-
-
-
-    def mget_frame():
-        try:
-            frame=camera.get_frame()
-            return frame       
-        except RuntimeError:
-            tools[2]=False
-            import sys
-            sys.exit(1)
-        finally:
-            if len(camera.error_counts):
-                camera_log[-1]=camera.error_log
-                if 5 in camera.error_counts:
-                    camera_log[-2]="camera"
-                rog(camera_log)
-
-
          
-      
 
-    nlog("カメラの確認を開始します")
-    # 左から、フェーズ、フェーズの中のフェーズ、時間、コーン検知、故障した部品、エラー文
-    camera_log=[6,1,None,False,None,None]
-    camera_log[2]=mget_time()
-    frame=mget_frame()
-    camera.frame_hozon(frame)
-
-
-    nlog("cdsの確認を開始します。")
-        #左から、フェーズ、時間、残り時間、明るさ、故障した部品、エラー文
-    cds_log=[-1,None,None,"high",None,None]
-    cds_log[1]=mget_time()
-    try:
-        cds.get_brightness()
-        cds_log[2]=cds.brightness
-    except:
-        tools[0]=False
-        import sys
-        sys.exit(1)
-    finally:
-        if len(cds.error_counts):
-            cds_log[4]=cds.error_log
-            if 5 in cds.error_counts:
-                cds_log[3]="cds"          
-            rog(cds_log)
-
-        sleep(1)
-
-    
-    nlog("motorの確認を開始します")
-    sleep(2)
-    mforward(5)
-    mturn_left(5)
-    mturn_right(5)
-    mbackward(5)
-    mstop()
-    sleep(2)
-    
-    nlog("servoの確認をします。")
-    sleep(2)
-    
-
-    #変更点 
-    try:            
-        servo.rotate()
-        sleep(5)
-        
-    except RuntimeError:
-        nlog("サーボモーターが使えなくなったため、コードを停止します。")
-        tools[4]=False
-        import sys
-        sys.exit(1)
-    finally:
-        if len(servo.error_counts):
-            #左から順にフェーズ、時間、故障した部品、エラー文
-            servo_log=[-2,mget_time(),None,None]
-            servo_log[3]=servo.error_log
-            if 5 in servo.error_counts:
-                servo_log[2]="servo"
-            rog(servo_log)
-
-    try:
-        servo.stop()
-    except RuntimeError:
-        nlog("サーボモーターが使えなくなったため、コードを停止します。")
-        tools[4]=False
-        import sys
-        sys.exit(1)
-    finally:
-        if len(servo.error_counts):
-            #左から順にフェーズ、時間、故障した部品、エラー文
-            servo_log=[-2,mget_time(),None,None]
-            servo_log[3]=servo.error_log
-            if 5 in servo.error_counts:
-                servo_log[2]="servo"
-            rog(servo_log)
 
 
     nlog("GPSの確認を開始します。")
@@ -448,20 +157,10 @@ try:
 
     rog(gps_log)
     nlog('gps取得しました')
-    
-    
-
-        
-                
-
-
        
-        
 
 finally:        
-    motors.release()
     gps.delete()
-    camera.release()
 
     if False in tools:
         nlog("故障した部品があるため、ラズパイをシャットダウンさせます。")
